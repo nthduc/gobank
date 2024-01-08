faba9a2490caf60adb44ed75750a0e7c39df1524
nthduc <nguyenthaiduc0212@gmail.com>
2024-01-08 22:09:29 +0700

    update 20:14

--- api.go
+++ api.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
+	jwt "github.com/golang-jwt/jwt/v4"
 	"github.com/gorilla/mux"
 )
 
 type apiFunc func(http.ResponseWriter, *http.Request) error
-type apiError struct {
+type ApiError struct {
 	Error string `json:"error"`
 }
 
@@ -34,7 +36,7 @@ func (s *APIServer) Run() {
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
 
-	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccountByID))
+	router.HandleFunc("/account/{id}", withJWTAuth(makeHTTPHandleFunc(s.handleGetAccountByID)))
 
 	router.HandleFunc("/transfer", makeHTTPHandleFunc(s.handleTransfer))
 
@@ -77,7 +79,7 @@ func (s *APIServer) handleGetAccountByID(w http.ResponseWriter, r *http.Request)
 		return writeJSON(w, http.StatusOK, account)
 	}
 	if r.Method == "DELETE" {
-		return s.handleDeleteAccount(w,r)
+		return s.handleDeleteAccount(w, r)
 	}
 	return fmt.Errorf("method not allowed %s", r.Method)
 }
@@ -92,6 +94,14 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 	if err := s.store.CreateAccount(account); err != nil {
 		return err
 	}
+
+	tokenString,err := createJWT(account)
+	if err != nil {
+		return err
+	}
+
+
+	fmt.Println("JWT Token: ", tokenString)
 
 	return writeJSON(w, http.StatusOK, account)
 }
@@ -114,7 +124,7 @@ func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error
 		return err
 	}
 	defer r.Body.Close()
-	return writeJSON(w,http.StatusOK,transferReq)
+	return writeJSON(w, http.StatusOK, transferReq)
 }
 
 func writeJSON(w http.ResponseWriter, status int, v any) error {
@@ -123,11 +133,50 @@ func writeJSON(w http.ResponseWriter, status int, v any) error {
 	return json.NewEncoder(w).Encode(v)
 }
 
+
+func createJWT(account *Account) (string, error){
+	// Create the Claims
+	claims := &jwt.MapClaims{
+	"expiresAt": 15000,
+	"accountNumber": account.Number,
+}
+	secret := os.Getenv("JWT_SECRET")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
+	return token.SignedString([]byte(secret))
+
+}
+func withJWTAuth(handleFunc http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("calling JWT auth middleware")
+		tokenString := r.Header.Get("x-jwt-token")
+		_,err := validateJWT(tokenString)
+		if err != nil {
+			writeJSON(w,http.StatusForbidden,ApiError{Error : "Invalid token"})
+		}
+		handleFunc(w, r)
+	}
+}
+
+const jwtServer = "nthduc1234"
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	secret := os.Getenv("JWT_SECRET")
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(secret), nil
+	})
+}
+
 func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if err := f(w, r); err != nil {
 			// handle the error
-			writeJSON(w, http.StatusBadRequest, apiError{Error: err.Error()})
+			writeJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
 
 		}
 	}
--- go.mod
+++ go.mod
@@ -2,7 +2,9 @@ module github.com/nthduc/gobank
 
 go 1.20
 
+require github.com/lib/pq v1.10.9
+
 require (
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/lib/pq v1.10.9
 )
